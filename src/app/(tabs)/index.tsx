f815af4010427d24dbce7a8d4fccdfdff46e1a1f
vrinch <odukwevince76@gmail.com>
2024-06-04 23:43:54 +0100

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/src/components/EditScreenInfo';
+import { Text, View } from '@/src/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Hello World when are you coming back here to me
+      </Text>
+      <View
+        style={styles.separator}
+        lightColor='#eee'
+        darkColor='rgba(255,255,255,0.1)'
+      />
+      <EditScreenInfo path='app/(tabs)/index.tsx' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: 'BoingSemiBold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
---
+++ src/constants/Colors.ts
@@ -0,0 +1,28 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const TINT_COLOR_LIGHT = '#0A7EA4';
+const TINT_COLOR_DARK = '#FFFFFF';
+
+const Colors = {
+  light: {
+    text: '#11181C',
+    background: '#FFFFFF',
+    tint: TINT_COLOR_LIGHT,
+    icon: '#687076',
+    tabIconDefault: '#687076',
+    tabIconSelected: TINT_COLOR_LIGHT,
+  },
+  dark: {
+    text: '#ECEDEE',
+    background: '#151718',
+    tint: TINT_COLOR_DARK,
+    icon: '#9BA1A6',
+    tabIconDefault: '#9BA1A6',
+    tabIconSelected: TINT_COLOR_DARK,
+  },
+};
+
+export default Colors;
